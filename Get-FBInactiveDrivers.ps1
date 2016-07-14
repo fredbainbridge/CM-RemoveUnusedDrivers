759b93eb8977f2ccfeec9af4323598ef2b2e694b
CM-Admin <CM-Admin>
2016-07-14 10:42:14 -0500

    fixed html output to show categories and packages of unused drivers

--- Get-FBInactiveDrivers.ps1
+++ Get-FBInactiveDrivers.ps1
@@ -237,13 +237,43 @@ set-location $env:SystemDrive
 
 if($HTMLReport)
 {
-    $a = "<style>"
+    <#$a = "<style>"
     $a = $a + "BODY{background-color:white;}"
     $a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;text-align: left;}"
     $a = $a + "TH{border-width: 1px;padding: 1px;border-style: solid;border-color: black;text-align: left;}"
     $a = $a + "TD{border-width: 1px;padding: 1px;border-style: solid;border-color: black;}"
     $a = $a + "</style>"
+    $RowExample = "<tr><td>Intel(R) Smart Sound Technology (Intel(R) SST) OED</td><td>16787493</td><td>IntcOED.inf</td><td>8.20.0.877</td><td>System.String[]</td><td>System.String[]</td></tr>"
+    #>
+    $html = 
+@"
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <style>BODY{background-color:white;}TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;text-align: left;}TH{border-width: 1px;padding: 1px;border-style: solid;border-color: black;text-align: left;}TD{border-width: 1px;padding: 1px;border-style: solid;border-color: black;}</style>
+    </head><body>
+    <H2>Unused Driver Information</H2>
+    <table>
+    <colgroup><col/><col/><col/><col/><col/><col/></colgroup>
+    <tr><th>Name</th><th>ID</th><th>InfFile</th><th>Version</th><th>Categories</th><th>Packages</th></tr>
+"@
 
-$UnusedDriverObjects | ConvertTo-Html -Head $a -body "<H2>Unused Driver Information</H2>"| out-file $HTMLReport -WhatIf:$false
+    $UnusedDriverObjects | ForEach-Object {
+        $tempPackages = $null
+        $tempCategories = $null
+        $PSItem.Packages | ForEach-Object {
+            $tempPackages = "$tempPackages, $PSItem"           
+        }
+        $tempPackages = $tempPackages.trim(", ")
+        $PSItem.Categories | ForEach-Object {
+            $tempCategories = "$tempCategories, $PSItem"
+        }
+        $tempCategories = $tempCategories.Trim(", ")
+        $HTML = "$HTML<tr><td>$($PSItem.Name)</td><td>$($PSItem.ID)</td><td>$($PSItem.InfFile)</td><td>$($PSItem.Version)</td><td>$tempCategories</td><td>$tempPackages</td></tr>"
+    }
+    $html = "$HTML </table></body></html>"
+    $HTML | Out-File $HTMLReport -WhatIf:$false
+    #$UnusedDriverObjects | ConvertTo-Html -Head $a -body "<H2>Unused Driver Information</H2>"| out-file $HTMLReport -WhatIf:$false
+    
 }
 Stop-Transcript
