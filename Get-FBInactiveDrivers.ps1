918fe42f68f516ef07da31f25dde20f74b201c72
CM-Admin <CM-Admin>
2016-07-14 15:23:28 -0500

    fixed type with transcript output file

--- Get-FBInactiveDrivers.ps1
+++ Get-FBInactiveDrivers.ps1
@@ -12,7 +12,7 @@ param(
 
 import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1') -Verbose:$false
 
-Start-Transcript -Path "Get-FBInacticeDrivers.log" -Append -Force -WhatIf:$false
+Start-Transcript -Path "Get-FBInactiveDrivers.log" -Append -Force -WhatIf:$false
 
 set-location $SiteCode`:
 
